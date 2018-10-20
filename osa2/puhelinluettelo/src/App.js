d94d7abb7e93f67c04a3d63c33b6b2af56aff11f
Jari Avikainen <jari.avikainen@helsinki.fi>
2018-10-20 16:06:32 +0300

    ADD: Ex.2.9

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -5,10 +5,14 @@ class App extends React.Component {
     super(props)
     this.state = {
       persons: [
-        { name: 'Arto Hellas', number: '040-123456' }
+        { name: 'Arto Hellas', number: '040-123456' },
+        { name: 'Martti Tienari', number: '040-123456' },
+        { name: 'Arto Järvinen', number: '040-123456' },
+        { name: 'Lea Kutvonen', number: '040-123456' }
       ],
       newName: '',
-      newNumber: ''
+      newNumber: '',
+      filter: ''
     }
   }
 
@@ -46,10 +50,26 @@ class App extends React.Component {
     this.setState({ newNumber: event.target.value })
   }
 
+  handleFilterChange = (event) => {
+    console.log(event.target.value)
+    this.setState({ filter: event.target.value })
+  }
+
   render() {
+    const filteredPersons =
+      this.state.persons.filter(person => person.name.toLowerCase().search(this.state.filter.toLowerCase()) > -1)
+
     return (
       <div>
-        <h2>Puhelinluettelo</h2>
+        <h1>Puhelinluettelo</h1>
+        <div>
+          rajaa näytettäviä:
+          <input
+            value={this.state.filter}
+            onChange={this.handleFilterChange}
+          />
+        </div>
+        <h2>Lisää uusi</h2>
         <form onSubmit={this.addPerson}>
           <div>
             nimi:
@@ -72,7 +92,7 @@ class App extends React.Component {
         <h2>Numerot</h2>
         <table>
           <tbody>
-          {this.state.persons.map(person => <Person key={person.name} person={person} />)}
+          {filteredPersons.map(person => <Person key={person.name} person={person} />)}
         </tbody>
         </table>
       </div>
