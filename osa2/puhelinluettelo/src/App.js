1aaf123a104fae2a69afe9b7d4a3b7eb97cd42cb
Jari Avikainen <jari.avikainen@helsinki.fi>
2018-10-20 15:23:53 +0300

    ADD: Ex.2.6

---
+++ osa2/puhelinluettelo/src/App.js
@@ -0,0 +1,64 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      persons: [
+        { name: 'Arto Hellas' }
+      ],
+      newName: ''
+    }
+  }
+
+  addPerson = (event) => {
+    event.preventDefault()
+    const person = {
+      name: this.state.newName
+    }
+
+    const persons = this.state.persons.concat(person)
+
+    this.setState({
+      persons: persons,
+      newName: ''
+    })
+  }
+
+  handleNoteChange = (event) => {
+    console.log(event.target.value)
+    this.setState({ newName: event.target.value })
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Puhelinluettelo</h2>
+        <form onSubmit={this.addPerson}>
+          <div>
+            nimi:
+            <input
+              value={this.state.newName}
+              onChange={this.handleNoteChange}
+            />
+          </div>
+          <div>
+            <button type="submit">lisää</button>
+          </div>
+        </form>
+        <h2>Numerot</h2>
+        <dl>
+          {this.state.persons.map(person => <Person key={person.name} person={person} />)}
+        </dl>
+      </div>
+    )
+  }
+}
+
+const Person = ({ person })  => {
+  return (
+    <dt>{person.name}</dt>
+  )
+}
+
+export default App
