a34d24ef404dc23c5f79957907f452a89e85aa5c
Jari Avikainen <jari.avikainen@helsinki.fi>
2018-10-24 17:40:26 +0300

    ADD: Ex.4.2

--- osa4/blogi/index.js
+++ osa4/blogi/index.js
@@ -4,23 +4,11 @@ const bodyParser = require('body-parser')
 const morgan = require('morgan')
 const cors = require('cors')
 const mongoose = require('mongoose')
-
-const Blog = mongoose.model('Blog', {
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-module.exports = Blog
+const blogsRouter = require('./controllers/blogs')
 
 morgan.token('reqbody', (req) => {
   return JSON.stringify(req.body)
 })
-
-app.use(cors())
-app.use(bodyParser.json())
-app.use(morgan(':method :url :reqbody :status :res[content-length] - :response-time ms'))
 
 if ( process.env.NODE_ENV !== 'production' ) {
   require('dotenv').config()
@@ -28,27 +16,22 @@ if ( process.env.NODE_ENV !== 'production' ) {
 
 const mongoUrl = process.env.MONGODB_URI
 
-mongoose.connect(mongoUrl, { useNewUrlParser: true })
+mongoose
+  .connect(mongoUrl, { useNewUrlParser: true })
+  .then( () => {
+    console.log('connected to database', process.env.MONGODB_URI)
+  })
+  .catch(err => {
+    console.log(err)
+  })
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
+app.use(cors())
+app.use(bodyParser.json())
+app.use(morgan(':method :url :reqbody :status :res[content-length] - :response-time ms'))
 
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
+app.use('/api/blogs', blogsRouter)
 
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-
-const PORT = 3003
+const PORT = process.env.PORT || 3003
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
