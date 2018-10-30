f5643f7db7eb18a14dc7417545f67848fa9fd7d6
Jari Avikainen <jari.avikainen@helsinki.fi>
2018-10-30 21:17:58 +0200

    ADD: Ex.5.1-5.2

---
+++ osa5/bloglist-frontend/src/App.js
@@ -0,0 +1,103 @@
+import React from 'react'
+import Blog from './components/Blog'
+import blogService from './services/blogs'
+import loginService from './services/login'
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      blogs: [],
+      username: '',
+      password: '',
+      user: null,
+      error: null,
+    }
+  }
+
+  componentDidMount() {
+    blogService.getAll().then(blogs =>
+      this.setState({ blogs })
+    )
+
+    const loggedUserJSON = window.localStorage.getItem('loggedBloglistUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      this.setState({ user })
+    }
+  }
+
+  login = async (event) => {
+    event.preventDefault()
+    try {
+      const user = await loginService.login({
+        username: this.state.username,
+        password: this.state.password
+      })
+
+      window.localStorage.setItem('loggedBloglistUser', JSON.stringify(user))
+      this.setState({ username: '', password: '', user })
+    } catch(exception) {
+      this.setState({
+        error: 'invalid username or password',
+      })
+      setTimeout(() => {
+        this.setState({ error: null })
+      }, 5000)
+    }
+  }
+
+  logout = (event) => {
+    window.localStorage.removeItem('loggedBloglistUser')
+    this.setState({ username: '', password: '', user: null })
+  }
+
+  handleLoginFieldChange = (event) => {
+    this.setState({ [event.target.name]: event.target.value })
+  }
+
+  render() {
+    if (this.state.user === null) {
+      return (
+        <div>
+          <h2>Log in to application</h2>
+          <form onSubmit={this.login}>
+            <div>
+              username
+              <input
+                type='text'
+                name='username'
+                value={this.state.username}
+                onChange={this.handleLoginFieldChange}
+              />
+            </div>
+            <div>
+              password
+              <input
+                type='password'
+                name='password'
+                value={this.state.password}
+                onChange={this.handleLoginFieldChange}
+              />
+            </div>
+            <button type='submit'>login</button>
+          </form>
+        </div>
+      )
+    }
+    return (
+      <div>
+        <h2>blogs</h2>
+        <p>
+          {this.state.user.name} logged in
+          <button onClick={this.logout}>logout</button>
+        </p>
+        {this.state.blogs.map(blog =>
+          <Blog key={blog._id} blog={blog}/>
+        )}
+      </div>
+    );
+  }
+}
+
+export default App;
