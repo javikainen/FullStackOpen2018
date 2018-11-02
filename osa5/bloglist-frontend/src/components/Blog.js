3847d5f02804a6a8bdcd720899feec4b4d08cf3e
Jari Avikainen <jari.avikainen@helsinki.fi>
2018-11-02 14:50:40 +0200

    ADD: Ex.5.6

--- osa5/bloglist-frontend/src/components/Blog.js
+++ osa5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,42 @@
 import React from 'react'
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+
+class Blog extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      visible: false
+    }
+  }
+
+  toggleVisibility = () => {
+    this.setState({ visible: !this.state.visible })
+  }
+
+  render() {
+    const blogStyle = {
+      paddingTop: 10,
+      paddingLeft: 2,
+      border: 'solid',
+      borderWidth: 1,
+      marginBottom: 5
+    }
+    const showWhenVisible = { display: this.state.visible ? '' : 'none' }
+
+    return (
+      <div style={blogStyle}>
+        <div onClick={this.toggleVisibility}>
+          {this.props.blog.title} {this.props.blog.author}
+        </div>
+        <div style={showWhenVisible}>
+          <ul style={{ 'list-style-type': 'none' }}>
+            <li><a href={this.props.blog.url}>{this.props.blog.url}</a></li>
+            <li>{this.props.blog.likes} likes <button>Like</button></li>
+            <li>Added by {this.props.blog.user.name}</li>
+          </ul>
+        </div>
+      </div>
+    )
+  }
+}
 
 export default Blog
