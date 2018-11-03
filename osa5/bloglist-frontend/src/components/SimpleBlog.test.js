2ccaf52e4fce8ed656e4aad8e74eb6e8f5eb28d9
Jari Avikainen <jari.avikainen@helsinki.fi>
2018-11-03 12:46:41 +0200

    ADD: Ex.5.13

--- osa5/bloglist-frontend/src/components/SimpleBlog.test.js
+++ osa5/bloglist-frontend/src/components/SimpleBlog.test.js
@@ -3,18 +3,32 @@ import { shallow } from 'enzyme'
 import SimpleBlog from './SimpleBlog'
 
 describe.only('<SimpleBlog />', () => {
-  it('renders title, author and likecount', () => {
-    const blog = {
-      title: 'This is a test title',
-      author: 'This is an imaginary author',
-      likes: 42
-    }
+  let blogComponent
 
-    const blogComponent = shallow(<SimpleBlog blog={blog} />)
+  const blog = {
+    title: 'This is a test title',
+    author: 'This is an imaginary author',
+    likes: 42
+  }
+  const mockHandler = jest.fn()
+
+  beforeEach(() => {
+    blogComponent = shallow(<SimpleBlog blog={blog} onClick={mockHandler}/>)
+  })
+
+  it('renders title, author and likecount', () => {
     const titleAuthorDiv = blogComponent.find('.titleAuthor')
     const likeCountDiv = blogComponent.find('.likes')
 
     expect(titleAuthorDiv.text()).toContain(`${blog.title} ${blog.author}`)
     expect(likeCountDiv.text()).toContain(blog.likes)
+  })
+
+  it('clicking the like button twice calls the event handler twice', () => {
+    const button = blogComponent.find('button')
+    button.simulate('click')
+    button.simulate('click')
+
+    expect(mockHandler.mock.calls.length).toBe(2)
   })
 })
